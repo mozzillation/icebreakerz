16bada33e8e4b76f9c259027b5ec56ac59dfacbd
Giuliano Mozzillo <giulianomozzillo@live.it>
2021-07-27 11:29:05 +0200

    Global Types added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    serverRuntimeConfig: {
-        PROJECT_ROOT: __dirname
-    }
+  webpack: function (config) {
+    config.module.rules.push(
+      {
+        test: /\.ya?ml$/,
+        use: 'js-yaml-loader',
+      },
+    )
+    return config
+  }
 }
--- utils/cards.ts
+++ utils/cards.ts
@@ -4,12 +4,9 @@ import process from 'process'
 import * as yaml from 'js-yaml'
 import { shuffleArray } from './functions'
 import getConfig from 'next/config'
-
+import cards from '../data/cards.yml'
 
 const getCards = () => {
-	const { serverRuntimeConfig } = getConfig()
-	const file = fs.readFileSync(path.join(serverRuntimeConfig.PROJECT_ROOT, './data/cards.yml'))
-	const cards = yaml.load(file)
 	let temporaryCards = shuffleArray(cards)
 	temporaryCards = temporaryCards.slice(0, 5)
 	return temporaryCards
