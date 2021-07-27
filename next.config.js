bf9081917a07e2809a00f4a889e0fc10cdc780b6
Giuliano Mozzillo <giulianomozzillo@live.it>
2021-07-27 11:13:16 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      type: 'json',
+      use: 'yaml-loader'
+    })
+    return config
+  }
+}
